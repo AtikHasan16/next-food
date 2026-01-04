2b1613e84b0fd5c3258f3a8eff8cdf1724b6a173
Hasan MD Atik <akhasannaeem@gmail.com>
2026-01-04 10:11:49 +0600

    style: remove background color from AboutPage and LocationsSection for consistency

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import AboutUsSection from "@/components/AboutUsSection";
 
 export default function AboutPage() {
   return (
-    <main className="min-h-screen bg-stone-900 text-white">
+    <main className="min-h-screen  text-white">
       <AboutUsSection />
     </main>
   );
--- src/app/locations/page.tsx
+++ src/app/locations/page.tsx
@@ -2,7 +2,7 @@ import LocationsSection from "@/components/LocationsSection";
 
 export default function LocationsPage() {
   return (
-    <main className="min-h-screen bg-stone-800 text-white">
+    <main className="min-h-screen text-white">
       <LocationsSection />
     </main>
   );

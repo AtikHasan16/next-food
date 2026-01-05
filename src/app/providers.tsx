4782539c9c15eaa2926f18feaceb08218348bcfa
Hasan MD Atik <akhasannaeem@gmail.com>
2026-01-05 17:57:28 +0600

    feat: update dependencies and integrate Toaster component for notifications

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,10 +1,24 @@
 "use client";
 
 import { HeroUIProvider } from "@heroui/react";
-
+import { Toaster } from "sonner";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <HeroUIProvider>{children}</HeroUIProvider>;
+  return (
+    <HeroUIProvider>
+      <Toaster
+        position="bottom-center"
+        toastOptions={{
+          style: {
+            background: "black",
+            color: "skyblue",
+            border: "1px solid skyblue",
+          },
+        }}
+      ></Toaster>
+      {children}
+    </HeroUIProvider>
+  );
 };
 
 export default Providers;
--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,11 +1,25 @@
+"use client";
 import { Button } from "@heroui/react";
-import React from "react";
+import React, { useState } from "react";
+import { toast } from "sonner";
 
 const AddToCartButton = () => {
+  const [carting, setCarting] = useState<boolean>(false);
+  const handleAdd = () => {
+    setCarting(true);
+    return toast.success("Added to cart!");
+  };
+
   return (
     <div>
-      <Button className="w-full" color="primary" variant="solid">
-        Add to Cart
+      <Button
+        onPress={handleAdd}
+        className="w-full"
+        color={!carting ? "primary" : "success"}
+        variant="solid"
+        disabled={carting}
+      >
+        {carting ? "Added" : "Add to Cart"}
       </Button>
     </div>
   );
